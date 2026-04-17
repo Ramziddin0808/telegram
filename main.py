67c661d59452a7e9819a9c2bb8744ca5ccbf527d
Ramziddin0808 <rayimovramziddin761@gmail.com>
2026-04-17 21:18:40 +0500

    yangi

--- main.py
+++ main.py
@@ -422,6 +422,7 @@ async def router(message: Message):
             ydl_opts = {
                 "format": "best[height<=480]",
                 "outtmpl": filename,
+                'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                 "noplaylist": True,
                 'cookiefile': 'cookies.txt',
                     'format': 'best',
